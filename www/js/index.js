eeef609a7bd47cb32a9eb80b628c6317479ddf32
JohnKal <jkalimer13@gmail.com>
2015-10-22 20:25:01 +0300

    After the splash screen display a login screen CIT-138, CIT-139, CIT-141

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -35,13 +36,21 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
         console.log("device is ready");
-        setTimeout(function () {
-            myNavigator.pushPage('start.html', {animation: "fade"});
 
-        }, 3000)
+        var login = checkLogin();
+        if (login) {
+          myNavigator.pushPage('account.html', {animation: "fade"});
+        } else {
+          setTimeout(function () {
+            myNavigator.pushPage('login.html', {animation: "fade"});
+
+          }, 3000)
+        }
+        console.log("start.html");
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -53,9 +62,100 @@ var app = {
     }
 };
 
-app.initialize();
+app.initialize(); 
 
-ons.bootstrap();
-      ons.ready(function() {
-        console.log("onse is ready");
+var module = ons.bootstrap('app', ['onsen']);
+module.controller('AppController', function($scope) {
+  console.log("Angular ready!!!");
+  ons.ready(function() {
+    console.log("onsen ready!!!");
+  });
+  $scope.logIn = function (username, password) {
+    console.log("button pressed!");
+    validateLogin (username, password);
+  }
+  $scope.logOut = function () {
+    console.log("logout pressed!");
+    localStorage.removeItem("logintoken");
+    myNavigator.pushPage('login.html', {animation: "fade"});
+  } 
+  $scope.register = function (username, email, password) {
+    console.log("register pressed!");
+    registration(username, email, password);
+  }
+});
+
+function validateLogin(username, password) {
+    if (username == "giannis" && password == "giannis") {
+        console.log("Login completed");
+        modal.show();
+        setTimeout(function () {
+          modal.hide();
+          saveLocalStorage();
+          myNavigator.pushPage('account.html', {animation: "fade"});
+        }, 2000)
+    } else {
+        console.log("Login failed!");
+        ons.notification.alert({
+          message: 'Check your username or password.',
+          // or messageHTML: '<div>Message in HTML</div>',
+          title: 'Login Failed',
+          buttonLabel: 'OK',
+          animation: 'default',
+          callback: function() {
+          }
         });
+    } 
+}
+
+function saveLocalStorage() {
+  console.log("typeofStorage " + typeof(Storage));
+  if(typeof(Storage) !== "undefined") {
+      localStorage.setItem("logintoken", true);
+  } else {
+
+  }
+}
+
+function registration(username, email, password) {
+    var email_validation = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    if (username && email && password) {
+      if (email_validation.test(email)){
+        console.log("register ok");
+        document.getElementById("email").style.display = "none";
+        ons.notification.alert({
+          message: 'Registration completed!',
+          title: 'message',
+          buttonLabel: 'OK',
+          animation: 'default',
+          callback: function() {
+          }
+        });
+      } else {
+        document.getElementById("email").style.display = "inline";
+      }
+    } else {
+      if (email_validation.test(email)){
+        document.getElementById("email").style.display = "none";
+      } else {
+        document.getElementById("email").style.display = "inline";
+      }
+      ons.notification.alert({
+          message: 'Check your username, email or password.',
+          title: 'Registration Failed',
+          buttonLabel: 'OK',
+          animation: 'default',
+          callback: function() {
+          }
+        });
+    }
+}
+
+function checkLogin() {
+  if(localStorage.getItem("logintoken") === null) {
+      return false;
+  } else if (localStorage.getItem("logintoken")) {
+      return true;
+  }
+}
+
