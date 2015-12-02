235356f2408b4e1750e4077f0ae742060c642b40
Alexandros Tzoumas <alexandros.tzoumas@gmail.com>
2015-12-02 12:46:40 +0200

    create signed file from android studio

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -347,15 +347,37 @@ function getMissions() {
         var response = JSON.parse(xhttp.responseText)
         if (response.status == "success") {
           //Εδώ δοκίμασα να παράγω το grid και να το κολλήσω στην html.
-            var gridview = document.createElement("div").setAttribute('id', 'gridview');
             var newdiv = document.createElement("div");
-          newdiv.innerHTML = "<ons-row> <ons-col id='grid-column' width='46%'><img src='http://cityrus.projects.development1.scify.org/www/city-r-us-web/public/img/mission.png' alt='scify' height='100' width='100' align='middle'> <p id='mission_description'>Mission1 Description</p> <p id='contributions'>12 contributors</p> </ons-col> <ons-col id='grid-column' width='46%'><img src='http://cityrus.projects.development1.scify.org/www/city-r-us-web/public/img/mission.png' alt='scify' height='100' width='100' align='middle'> <p id='mission_description'>Mission2 Description</p> <p id='contributions'>12 contributors</p> </ons-col> </ons-row>";
+          /*newdiv.innerHTML = "<ons-row> <ons-col id='grid-column' width='46%'><img src='http://cityrus.projects.development1.scify.org/www/city-r-us-web/public/img/mission.png' alt='scify' height='100' width='100' align='middle'> <p id='mission_description'>Mission1 Description</p> <p id='contributions'>12 contributors</p> </ons-col> <ons-col id='grid-column' width='46%'><img src='http://cityrus.projects.development1.scify.org/www/city-r-us-web/public/img/mission.png' alt='scify' height='100' width='100' align='middle'> <p id='mission_description'>Mission2 Description</p> <p id='contributions'>12 contributors</p> </ons-col> </ons-row>";
           document.getElementById("gridview").appendChild(newdiv);
+*/
 
           //Εδώ κάνω μια επανάληψη τον πίνακα των αποστολών για να πάρω περιγραφές και τα σχετικά.
           for (var i = 0; i < response.message.missions.length; i++) {
-            console.log(response.message.missions[i].description);
+
+console.log('aaa')
+              newdiv.innerHTML = "<ons-row> " +
+              "<ons-col  width='46%'>" +
+              "<img src='http://cityrus.projects.development1.scify.org/www/city-r-us-web/public/img/mission.png' alt='scify' height='100' width='100' align='middle'> " +
+              "<p class='mission_description'>Mission1 Description</p> " +
+              "<p class='contributions'>12 contributors</p> " +
+              "</ons-col> " +
+              "<ons-col  width='46%'>" +
+              "<img src='http://cityrus.projects.development1.scify.org/www/city-r-us-web/public/img/mission.png' alt='scify' height='100' width='100' align='middle'> " +
+              "<p class='mission_description'>Mission2 Description</p>" +
+              " <p class='contributions'>12 contributors</p> " +
+              "</ons-col> " +
+              "</ons-row>";
+
+
+
           }
+
+
+            document.getElementById("gridview").appendChild(newdiv);
+
+
+
         } else {
           ons.notification.alert({
             message: "" + response.message.description,
